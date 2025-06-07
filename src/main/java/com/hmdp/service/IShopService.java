0c522aa4d5b30a846c2ed04d072ec0e9be22e1cb
jupiter-water-wood <2294249395@qq.com>
2025-06-07 16:34:24 +0800

    添加商户查询缓存，解决缓存击穿、雪崩等问题

--- src/main/java/com/hmdp/service/IShopService.java
+++ src/main/java/com/hmdp/service/IShopService.java
@@ -1,5 +1,6 @@
 package com.hmdp.service;
 
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Shop;
 import com.baomidou.mybatisplus.extension.service.IService;
 
@@ -13,4 +14,18 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface IShopService extends IService<Shop> {
 
+    /**
+     * 根据id查询商户
+     * @param id
+     * @return
+     */
+    Result queryById(Long id);
+
+    /**
+     * 更新商铺
+     *
+     * @param shop
+     * @return
+     */
+    Result update(Shop shop);
 }
--- src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
@@ -1,10 +1,19 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONUtil;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.ShopType;
 import com.hmdp.mapper.ShopTypeMapper;
 import com.hmdp.service.IShopTypeService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import io.netty.util.internal.StringUtil;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * <p>
@@ -17,4 +26,28 @@ import org.springframework.stereotype.Service;
 @Service
 public class ShopTypeServiceImpl extends ServiceImpl<ShopTypeMapper, ShopType> implements IShopTypeService {
 
+    @Resource
+    StringRedisTemplate stringRedisTemplate = new StringRedisTemplate();
+    @Override
+    public List<ShopType> listShops() {
+        // 1.从redis页面获取商铺类型
+        String shops = stringRedisTemplate.opsForValue().get("shops:");
+
+        // 2.存在，直接返回
+        if (StrUtil.isNotBlank(shops)) {
+            List<ShopType> list = JSONUtil.toList(shops, ShopType.class);
+            return list;
+        }
+
+        // 3.不存在，调用数据库获取
+        List<ShopType> list = query().orderByAsc("sort").list();
+
+        // 4.将商铺类型存储到redis中
+        if (!list.isEmpty()) {
+            stringRedisTemplate.opsForValue().set("shops:", JSONUtil.toJsonStr(list));
+        }
+
+        // 5.返回商铺类型
+        return list;
+    }
 }
